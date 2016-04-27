92c9dbc19736da393cfad1bb6980e9f3dbb1382b
Samanta Cecília de Barros Souza <samanta@MAC7448.local>
2016-04-27 13:10:07 -0300

    Fix main in api.py

--- api.py
+++ api.py
@@ -58,5 +58,5 @@ api.add_resource(TodoList, '/todos')
 api.add_resource(Todo, '/todos/<todo_id>')
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', threaded=True)
